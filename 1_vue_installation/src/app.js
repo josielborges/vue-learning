0764264eef0ef93eee18146a3cc60a2fe6218a11
Josiel Borges <chrys.jo@gmail.com>
2024-07-15 20:47:55 -0300

    Inputs and Data Binding

--- 1_vue_installation/src/app.js
+++ 1_vue_installation/src/app.js
@@ -2,7 +2,15 @@ const MyNameApp = {
     data() {
         return {
             name: 'Josiel',
-            age: 35
+            age: 35,
+            input_name: ""
+        }
+    },
+    methods:{
+        submitForm(e){
+            e.preventDefault();
+            this.name = this.input_name;
+            console.log(this.input_name)
         }
     },
 }
